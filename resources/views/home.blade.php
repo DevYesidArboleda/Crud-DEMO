84ef4357ee31689b1fe0a42e4f84ae59aa3dbb8d
jhonniermosquera <jmosquera@softdreams.co>
2018-08-04 15:47:18 -0500

    Se agrega el formulario para cargar el archivo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,10 +33,24 @@
                 <div class="modal-content">
                   <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Modal Header</h4>
+                    <h4 class="modal-title">Cargar comprobante</h4>
                   </div>
                   <div class="modal-body">
-                    <p>Some text in the modal.</p>
+                    <form method="POST" class="form-horizontal" action="" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                          <label for="" class="col-md-4 control-label">Nuevo archivo</label>
+                          <div class="col-md-6">
+                            <input type="file" name="file" id="file" class="form-control"></input>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="btn btn-info pull-right">Enviar</button>
+                          </div>
+                          
+                        </div>
+                    </form>
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
