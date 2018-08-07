a3e82e7178a2e39649768a44afd170ae2426376d
jhonniermosquera <jmosquera@softdreams.co>
2018-08-07 14:35:28 -0500

    Nuevos cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -46,7 +46,7 @@
                         </div>
                         <div class="form-group">
                           <div class="col-md-6 col-md-offset-4">
-                            <button type="submit" class="btn btn-info pull-right">Enviar</button>
+                            <button type="submit" class="btn btn-primary pull-right">Enviar</button>
                           </div>
                           
                         </div>
