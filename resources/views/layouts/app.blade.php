8098844ef6c20ba61eb24d2db2cc39334038383d
Yesid-Dev <yesison714@gmail.com>
2018-08-10 20:50:25 -0500

    Mensajes flash timeout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,5 +94,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script>
+        $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
+    </script>
 </body>
 </html>
