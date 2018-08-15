c8f48e9b699bfb601b7a13a2ff2c6f7b21c18849
jhonniermosquera <jmosquera@softdreams.co>
2018-08-15 10:53:59 -0500

    Add Country, department, city and customer Model, migration, route and views

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -27,12 +16,41 @@ Route::get('formulario', 'FileController@index');
 
 Route::post('public', 'FileController@store');
 
-//Panel de ADMON
+//Panel de ADMON//
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function (){
+	//USER//
 	Route::resource('users','UsersController');
 	Route::get('users/{id}/destroy', [
             'uses' => 'UsersController@destroy',
             'as' => 'admin.users.destroy'
         ]);
+
+	//COUNTRY//
+	Route::resource('countries','CountryController');
+	Route::get('countries/{id}/destroy', [
+            'uses' => 'CountryController@destroy',
+            'as' => 'admin.countries.destroy'
+        ]);
+
+	//DEPARTMENT//
+	Route::resource('departments','DepartmentController');
+	Route::get('departments/{id}/destroy', [
+            'uses' => 'DepartmentController@destroy',
+            'as' => 'admin.departments.destroy'
+        ]);
+
+	//CITY//
+	Route::resource('cities','CityController');
+	Route::get('cities/{id}/destroy', [
+            'uses' => 'CityController@destroy',
+            'as' => 'admin.cities.destroy'
+        ]);
 });
+
+//CUSTOMER//
+Route::resource('customers','CustomerController');
+	Route::get('customers/{id}/destroy', [
+            'uses' => 'CustomerController@destroy',
+            'as' => 'customers.destroy'
+        ]);
 
